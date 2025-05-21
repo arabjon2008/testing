e7bb0bfe9dfd41908ca64aa8654bdefb4cee0057
arabjon2008 <eshmatovarabjon1@gmail.com>
2025-05-21 10:35:01 +0300

    kodlar yozildi

---
+++ app.js
@@ -0,0 +1,113 @@
+// let studentAge = prompt("Yoshingizni kiriting");
+// console.log(studentAge);
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// function myForEach() {
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+//   }
+// }
+// myForEach(() => {});
+
+// const arr = [1, 2, 3, 4, 5];
+
+// function myForEach() {
+//   for (let i = 0; i < arr.length; i++) {
+//     const arguments = arr[i];
+//     console.log(i);
+//   }
+// }
+// myForEach(() => {});
+
+// const adder = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const separation = (num1, num2) => {
+//   return num1 - num2;
+// };
+
+// const multiplication = () => {
+//   return num1 * num2;
+// };
+
+// const divide = () => {
+//   return num1 / num2;
+// };
+
+// function calculator(number1, number2, mathematic_taype) {
+//   switch (mathematic_taype) {
+//     case "+": {
+//       const response = adder(number1, number2);
+//       console.log(response);
+//       break;
+//     }
+//     case "-": {
+//       const response = separation(number1, number2);
+//       console.log(response);
+//       break;
+//     }
+//     case "*": {
+//       const response = multiplication(number1, number2);
+//       console.log(response);
+//       break;
+//     }
+//     case "/": {
+//       const response = divide(number1, number2);
+//       console.log(response);
+//       break;
+//     }
+//   }
+// }
+
+// calculator(9, 8, "+");
+
+// const phons = [
+//   { name: "iphone6", priase: 1000 },
+//   { name: "iphone7", priase: 1200 },
+//   { name: "iphone8", priase: 1300 },
+// ];
+
+// function find_phones(phons, priase) {
+//   const response = phons.filter((value) => {
+//     if (value.priase < priase) {
+//       return true;
+//     }
+//   });
+//   console.log(response);
+// }
+
+// find_phones(phons, 1100);
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// // }
+// let number = 10;
+// function convert(number) {
+//   console.log("O'nlik: " + number);
+//   console.log("Ikkilik: " + number.toString(2));
+// }
+// convert(number);
+
+// const user = {
+//   name: "Ali",
+//   age: 25,
+//   address: {
+//     city: "Toshkent",
+//     country: "O'zbekiston",
+//   },
+// };
+// const {
+//   name,
+//   address: { city },
+// } = user;
+// console.log(name);
+// console.log(city);
+
+// function findMax(numbers) {
+//   return Math.max(...numbers);
+// }
+// console.log(findMax([3, 5, 7, 2, 8, 1]));
+
+console.log(1);
